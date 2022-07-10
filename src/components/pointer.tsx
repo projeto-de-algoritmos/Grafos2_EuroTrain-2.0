b44b64820ac82368361a0dd792dac7e8346ad0c5
Samuel Nogueira Bacelar <samuelnbacelar@hotmail.com>
2022-07-10 12:04:23 -0300

    Arrumando pointer component

--- src/components/pointer.tsx
+++ src/components/pointer.tsx
@@ -1,9 +1,24 @@
+import dayjs from 'dayjs'
 import {HiArrowNarrowDown} from 'react-icons/hi'
+import { Col, Row } from 'reactstrap'
 
-export default function Pointer({time}) {
+export default function Pointer({time}: {time?: any}) {
+  if (time) {
+    const minutes = time * 60;
+    time = `${dayjs.duration(minutes, 'minutes').format(`H [horas]\nm [minutos]`)}`;
+  }
   return (
-  <div className="align-middle" style={{margin: '0 .8em'}}>
-    <HiArrowNarrowDown size='50' />
+  <div style={{margin: '0 .8em'}}>
+    <Row className="row justify-content-center">
+      <Col xs='auto'>
+        <HiArrowNarrowDown size='50' />
+      </Col>
+      {
+        !!time ?(<Col xs='auto'>{time}</Col>) : (<></>)
+      }
+
+    </Row>
+
   </div>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,11 @@ import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import type { AppProps } from 'next/app'
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration'
+import relativeTime from 'dayjs/plugin/relativeTime'
+dayjs.extend(duration);
+dayjs.extend(relativeTime)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
